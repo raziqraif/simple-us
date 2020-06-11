79f8a101223b4705bff7d0ad82494b1af7a63980
Raziq Raif Ramli <42981908+raziqraif@users.noreply.github.com>
2020-06-11 02:30:22 -0400

    Initial code for new tab look

--- simple-us/app/application.py
+++ simple-us/app/application.py
@@ -8,32 +8,41 @@ from manage import ManageTab
 from manage import ManageTabView
 from IPython.display import display
 
+from ipymaterialui import Tab
+from ipymaterialui import Tabs
 
-class AppView(Tab):
+class AppView(Tabs):
     """ View class for the main application """
 
     def __init__(self, controller):
-        super(Tab, self).__init__()
+        super(Tabs, self).__init__()
 
+        self.value = 0
+        self.centered = True
         self.controller = controller
 
         # TODO: Replace these with proper widgets.
         self.create = Box()
-        self.experiment = ManageTab().view
+        self.manage = ManageTab().view
         self.view = Box()
         self.about = Box()
 
         self.children = [
-            self.create,
-            self.experiment,
-            self.view,
-            self.about
+            Tab(label="Create", children=[self.create]),
+            Tab(label="Manage", children=[self.manage]),
+            Tab(label="View", children=[self.view]),
+            Tab(label="About", children=[self.about]),
+            # self.manage
+            # self.create,
+            # self.experiment,
+            # self.view,
+            # self.about
         ]
 
-        self.set_title(0, "Create")
-        self.set_title(1, "Manage")
-        self.set_title(2, "View")
-        self.set_title(3, "About")
+        # self.set_title(0, "Create")
+        # self.set_title(1, "Manage")
+        # self.set_title(2, "View")
+        # self.set_title(3, "About")
 
 
 class App:
