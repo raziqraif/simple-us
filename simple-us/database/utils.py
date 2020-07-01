7bba4f96762d7e3ac43b7743a18aa7b9d79960e6
Raziq Raif Ramli <42981908+raziqraif@users.noreply.github.com>
2020-07-01 09:15:04 -0400

    Update path to job directory.

--- simple-us/database/utils.py
+++ simple-us/database/utils.py
@@ -1,9 +1,19 @@
 # This module contains dummy data/methods for now
 
+import os
 from pathlib import Path
 
 # TODO: Update this when porting to jupyter notebook on mygeohub
-PRIVATE_JOBS_DIR = Path("C:\\Users\\muhdr\\Documents\\simple-us\\job")
+try:
+    HOSTNAME = os.environ["HOSTNAME"]
+except:
+    HOSTNAME = ""
+
+if "mygeohub" in HOSTNAME[:8]:
+    PRIVATE_JOBS_DIR = Path("~/simpleUSRun/job")
+else:
+    # TODO: Update your local job directory
+    PRIVATE_JOBS_DIR = Path("C:\\Users\\muhdr\\Documents\\simple-us\\job")
 SHARED_JOBS_DIR = Path("Dummy path")
 
 # Column number in database
