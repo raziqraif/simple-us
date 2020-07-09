56c3b6cfddcb5cb719b54e64a2d639e10eea6ca8
Raziq Raif Ramli <42981908+raziqraif@users.noreply.github.com>
2020-07-09 13:31:51 -0400

    View page func. update.

--- simple-us/view/controller.py
+++ simple-us/view/controller.py
@@ -1,5 +1,8 @@
 from typing import List
+from typing import Optional
+from typing import Tuple
 
+from .context import ViewContext
 from .sidebar import Sidebar
 from model import Experiment
 
@@ -7,10 +10,43 @@ from model import Experiment
 class ViewTab:
     def __init__(self):
         from .view import ViewTabUI
-        self.sidebar = Sidebar()
-        self.view = ViewTabUI(self, self.sidebar.view)
-        self.experiments: List[Experiment] = []
+        self._sidebar = Sidebar()
+        self._sidebar.visualize_variables = self.visualize_variables
+        self.contexts: List[ViewContext] = []
+        self.active_context: Optional[ViewContext] = None
 
-    def refresh_sidebar(self):
-        self.sidebar.experiments = self.experiments
-        self.sidebar.refresh()
+        self.view = ViewTabUI(self, self._sidebar.view)
+
+    @property
+    def active_tab_model(self) -> any:
+        return self.active_context
+
+    def maps_from_model(self, tab_model: any) -> any:
+        assert isinstance(tab_model, ViewContext)
+        return tab_model.maps
+
+    def map_titles_from_model(self, tab_model: any) -> List[str]:
+        assert isinstance(tab_model, ViewContext)
+        return tab_model.map_titles
+
+    def save_maps_to_model(self, maps: List[any], tab_model: any):
+        assert isinstance(tab_model, ViewContext)
+        tab_model.maps = maps
+
+    def new_view(self, experiments: List[Experiment]):
+        """ New display/compare view"""
+
+        context = ViewContext(experiments)
+        self.contexts.append(context)
+        self._switch_context(context)
+        self.view.new_tab(context.title, context, context.is_comparison)
+
+    def _switch_context(self, context: ViewContext):
+        self._sidebar.switch_context(context)
+        if self.active_context:
+            self.active_context.maps = self.view.maps()
+        self.active_context = context
+
+    def visualize_variables(self, system_component: str, spatial_resolution: str, type_of_result: str,
+                            result_to_view: str, filter_range: Tuple[float, float]) -> None:
+        pass
--- simple-us/view/view.py
+++ simple-us/view/view.py
@@ -1,7 +1,9 @@
-from copy import copy
+from copy import copy, deepcopy
+from typing import List
+from typing import Optional
 
 from ipyleaflet import Map
-from ipymaterialui import Button
+from ipymaterialui import Button, Tabs, Tab
 from ipymaterialui import Container
 from ipymaterialui import FormControl
 from ipymaterialui import FormControlLabel
@@ -23,51 +25,214 @@ from utils import PRIMARY_COLOR_LIGHT
 from utils import PRIMARY_COLOR
 
 
+class TabWithModel(Tab):
+    def __init__(self, tab_model: any = None, **kwargs):
+        super().__init__(**kwargs)
+        self.tab_model: any = tab_model
+
+
 class ViewTabUI(Container):
 
-    def __init__(self, controller: ViewTab, sidebar: SidebarView, **kwargs):
+    def __init__(self, controller: ViewTab, sidebar: any, **kwargs):
         super().__init__(**kwargs)
 
+        self.controller: ViewTab = controller
+
+        self._tab_bar: Optional[Container] = None
+        self._sidebar: any = sidebar
+        self._maps_area: Optional[Container] = None
+        self._main_area: Optional[Container] = None
+        self._view_widgets_wrapper: Optional[Container] = None
+
+        self._map_title_full: Optional[CustomText]
+        self._map_title_top: Optional[CustomText] = None
+        self._map_title_bottom: Optional[CustomText] = None
+        self._map_wrapper_full: Optional[Container] = None
+        self._map_wrapper_top: Optional[Container] = None
+        self._map_wrapper_bottom: Optional[Container] = None
+
+        self._default_tab: Optional[TabWithModel] = None
+        self._empty_text: CustomText = \
+            CustomText("Experiments selected for display/compare will appear here.")
+        self._init_ui()
+
+    def _init_ui(self):
         self.style_ = {
             "display": "flex",
-            "flex-direction": "row",
-            "justify-content": "center",
+            "flex-direction": "column",
+            "justify-content": "flex-start",
             "align-items": "center",
             "align-self": "center",
             "padding": "0px 0px 0px 0px",
-            "width": "100%",
-            # "height": "480px",
-            "margin": "0px 0px 0px 0px",
+            "width": "90%",
+            "border": "1px solid " + PRIMARY_COLOR,
+            "height": "730px",
+            "margin": "40px 40px 40px 40px",
             # "border-radius": "8px",
             # "background": INNER_BACKGROUND_COLOR,
         }
+        self._build_tab_bar()
+        self._build_maps_area()
+        self._view_widgets_wrapper = Container(children=[self._sidebar, self._maps_area],
+                                               style_={
+                                                  "display": "flex",
+                                                  "flex-direction": "row",
+                                                  "justify-content": "center",
+                                                  "align-items": "center",
+                                                  "align-self": "center",
+                                                  "padding": "0px 0px 0px 0px",
+                                                  "margin": "0px 0px 0px 0px",
+                                                  # "height": "100%",
+                                              })
+        self._main_area = Container(children=[self._empty_text],
+                                    style_={
+                                       "display": "flex",
+                                       "flex-direction": "column",
+                                       "justify-content": "center",
+                                       "align-items": "center",
+                                       "align-self": "center",
+                                       "padding": "0px 0px 0px 0px",
+                                       "margin": "0px 0px 0px 0px",
+                                       "height": "100%",
+                                       "width": "100%",
+                                   })
+        self.children = [self._tab_bar, self._main_area]
 
-        self.controller = controller
-        self.sidebar = sidebar
-        self.mainbar = None
+    def _build_tab_bar(self):
+        self._default_tab = CustomText("", tag="span", style_={"font-weight": "bold"})
+        self._tab_bar = Tabs(
+            children=[self._default_tab],
+            style_={
+                "background": PRIMARY_COLOR,
+                # "height": "65px",
+                "padding": "0px 0px 0px 0px",
+                "width": "100%",
+                "display": "flex",
+                "flex-direction": "row",
+                "align-items": "center",
+            },
+            centered=False, tab_model=self.controller.active_tab_model
+        )
 
-        self.build_mainbar()
-        self.children = [self.sidebar, self.mainbar]
+    def _build_maps_area(self):
+        self._map_title_full = CustomText("", style_={"font-weight": "bold",
+                                                      "font-size": "14px",
+                                                      "margin": "0px 0px 8px 1px",
+                                                      "align-self": "flex-start",
+                                                      })
+        self._map_title_top = CustomText("", style_={"font-weight": "bold",
+                                                     "font-size": "14px",
+                                                     "margin": "0px 0px 8px 1px",
+                                                     "align-self": "flex-start",
+                                                     })
+        self._map_title_bottom = CustomText("", style_={"font-weight": "bold",
+                                                        "font-size": "14px",
+                                                        "margin": "0px 0px 8px 1px",
+                                                        "align-self": "flex-start",
+                                                        })
+        self._map_wrapper_full = self._create_map_wrapper(self._map_title_full)
+        self._map_wrapper_top = self._create_map_wrapper(self._map_title_top, False)
+        self._map_wrapper_bottom = self._create_map_wrapper(self._map_title_bottom, False)
+        initial_maps_area = Container(children=["test"],  # FIXME
+                                      style_={
+                                          "display": "flex",
+                                          "flex-direction": "column",
+                                          "justify-content": "center",
+                                          "align-items": "center",
+                                          "align-self": "center",
+                                          "padding": "0px 0px 0px 0px",
+                                          "margin": "0px 0px 0px 0px",
+                                          "width": "650px",
+                                          "height": "600px",
+                                          # "width": "100%",
+                                          # "height": "480px",
+                                      })
+        self._maps_area = initial_maps_area
 
-    def build_mainbar(self):
-        title = CustomText("Experiment 2: Test", style_={"font-weight": "bold",
-                                                         "font-size": "14px",
-                                                         "margin": "0px 0px 8px 1px",
-                                                         "align-self": "flex-start",
-                                                         })
-        map_ = Map(center=(39.5, -98.35), zoom=4)
-        map_.layout.width = "618px"
-        map_.layout.height = "570px"
-        map_.layout.border = "1px solid " + PRIMARY_COLOR
-        wrapper = Container(children=[title, map_],
+    def _create_map_wrapper(self, title, full_height=True) -> Container:
+        height = "100%" if full_height else "50%"
+        wrapper = Container(children=[title],
                             style_={
                                 "display": "flex",
                                 "flex-direction": "column",
                                 "justify-content": "center",
                                 "align-items": "center",
-                                "width": "618px",
-                                "height": "600px",
+                                # "width": "618px",
+                                "height": height,
                                 "padding": "1px 1px 1px 1px",
                                 "margin": "0px 0px 0px 24px",
                             })
-        self.mainbar = wrapper
+        return wrapper
+
+    def _create_empty_map(self, full_height=True):
+        height = "570px" if full_height else "270px"
+        layout = Layout(width="600px", height=height)
+        return Map(layout=layout, center=(39.5, -98.35), zoom=4)  # TODO: Update this
+
+    def new_tab(self, tab_name: str, tab_model: any, comparison: bool = False):
+        children = self._tab_bar.children
+        children = copy(children) if children[0] != self._default_tab else []  # copy() is needed.
+        # It seems like if the memory address of children is the same, the DOM will not get updated.
+
+        tab = TabWithModel(label=CustomText(tab_name,
+                                            style_={
+                                                "color": "#ffffff",
+                                                "padding": "0px 0px 0px 0px",
+                                            }), tab_model=tab_model, selected=True)
+        children.append(tab)
+        self._tab_bar.children = children
+        self._tab_bar.value = tab
+        if comparison:
+            maps = [self._create_empty_map(full_height=False) for count in range(2)]
+        else:
+            maps = [self._create_empty_map()]
+        self.controller.save_maps_to_model(maps, tab_model)
+        self._show_map(tab_model)
+
+    def _show_map(self, tab_model: any):
+        maps = self.controller.maps_from_model(tab_model)
+        assert 1 <= len(maps) <= 2
+        if len(maps) == 1:
+            self._show_full_map(tab_model)
+        elif len(maps) == 2:
+            self._show_comparison_maps(tab_model)
+
+    def _show_full_map(self, tab_model: any):
+        if self._main_area.children[0] == self._empty_text:
+            self._main_area.children = [self._view_widgets_wrapper]
+        title = self.controller.map_titles_from_model(tab_model)[0]
+        map_ = self.controller.maps_from_model(tab_model)[0]
+
+        self._map_title_full.children = title
+        self._map_wrapper_full.children = [self._map_title_full, map_]
+        self._maps_area.children = [self._map_wrapper_full]
+
+    def _show_comparison_maps(self, tab_model: any):
+        if self._main_area.children[0] == self._empty_text:
+            self._main_area.children = [self._view_widgets_wrapper]
+        titles = self.controller.map_titles_from_model(tab_model)
+        maps = self.controller.maps_from_model(tab_model)
+        self._map_title_top.children = titles[0]
+        self._map_title_bottom.children = titles[1]
+        self._map_wrapper_top.children = [self._map_title_top, maps[0]]
+        self._map_wrapper_bottom.children = [self._map_title_bottom, maps[1]]
+        self._maps_area.children = [self._map_wrapper_top, self._map_wrapper_bottom]
+
+    def close_tab(self, tab_model):
+        for tab in self._tab_bar.children:
+            if isinstance(tab, TabWithModel) and (tab.tab_model == tab_model):
+                children = copy(self._tab_bar.children)
+                children.remove(tab)
+                self._tab_bar.children = children
+
+    def maps(self) -> Optional[List[Map]]:
+        if self._view_widgets_wrapper.children[0] == self._empty_text:
+            return None
+
+        map_wrapper_1 = self._maps_area.children[0]
+        maps = [map_wrapper_1.children[1]]
+        if len(self._maps_area.children) == 2:
+            map_wrapper_2 = self._maps_area.children[1]
+            maps.append(map_wrapper_2)
+
+        return maps
